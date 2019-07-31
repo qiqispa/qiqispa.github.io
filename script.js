297443e8d1569eb8a8c94d0ce6406ecd2e5fc634
qiqispa <53331785+qiqispa@users.noreply.github.com>
2019-08-01 06:36:48 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,489 @@
+
+// 첫 번째 동영상
+
+     $('#play-pause-button').click(function () {
+
+     		$("#pre1").hide();
+
+		   var mediaVideo = $("#bgdvid").get(0);
+		   var playv = $("#playv");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 두 번째 동영상
+
+     $('#play-pause-button2').click(function () {
+
+     		$("#pre2").hide();
+
+		   var mediaVideo = $("#bgdvid2").get(0);
+		   var playv = $("#playv2");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 네 번째 동영상
+
+     $('#play-pause-button4').click(function () {
+
+     		$("#pre4").hide();
+
+		   var mediaVideo = $("#bgdvid4").get(0);
+		   var playv = $("#playv4");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 다섯 번째 동영상
+
+     $('#play-pause-button5').click(function () {
+
+     		$("#pre5").hide();
+
+		   var mediaVideo = $("#bgdvid5").get(0);
+		   var playv = $("#playv5");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 여섯 번째 동영상
+
+     $('#play-pause-button6').click(function () {
+
+     		$("#pre6").hide();
+
+		   var mediaVideo = $("#bgdvid6").get(0);
+		   var playv = $("#playv6");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 일곱 번째 동영상
+
+     $('#play-pause-button7').click(function () {
+
+     		$("#pre7").hide();
+
+		   var mediaVideo = $("#bgdvid7").get(0);
+		   var playv = $("#playv7");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 아홉 번째 동영상
+
+     $('#play-pause-button9').click(function () {
+
+     		$("#pre9").hide();
+
+		   var mediaVideo = $("#bgdvid9").get(0);
+		   var playv = $("#playv9");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 열 번째 동영상
+
+     $('#play-pause-button10').click(function () {
+
+     		$("#pre10").hide();
+
+		   var mediaVideo = $("#bgdvid10").get(0);
+		   var playv = $("#playv10");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 열한 번째 동영상
+
+     $('#play-pause-button11').click(function () {
+
+     		$("#pre11").hide();
+
+		   var mediaVideo = $("#bgdvid11").get(0);
+		   var playv = $("#playv11");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 열두 번째 동영상
+
+     $('#play-pause-button12').click(function () {
+
+     		$("#pre12").hide();
+
+		   var mediaVideo = $("#bgdvid12").get(0);
+		   var playv = $("#playv12");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 열세 번째 동영상
+
+     $('#play-pause-button13').click(function () {
+
+     		$("#pre13").hide();
+
+		   var mediaVideo = $("#bgdvid13").get(0);
+		   var playv = $("#playv13");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 열네 번째 동영상
+
+  //    $('#play-pause-button14').click(function () {
+
+  //    		$("#pre14").hide();
+
+		//    var mediaVideo = $("#bgdvid14").get(0);
+		//    var playv = $("#playv14");
+		//    if (mediaVideo.paused) {
+		//        mediaVideo.play();
+		//        playv.parent();
+		//    } else {
+		//        mediaVideo.pause();
+		//        playv.parent();
+		//   }
+		// });
+
+// 열다섯 번째 동영상
+
+  //    $('#play-pause-button15').click(function () {
+
+  //    		$("#pre15").hide();
+
+		//    var mediaVideo = $("#bgdvid15").get(0);
+		//    var playv = $("#playv15");
+		//    if (mediaVideo.paused) {
+		//        mediaVideo.play();
+		//        playv.parent();
+		//    } else {
+		//        mediaVideo.pause();
+		//        playv.parent();
+		//   }
+		// });
+
+// 열여섯 번째 동영상
+
+     $('#play-pause-button16').click(function () {
+
+     		$("#pre16").hide();
+
+		   var mediaVideo = $("#bgdvid16").get(0);
+		   var playv = $("#playv16");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 열일곱 번째 동영상
+
+  //    $('#play-pause-button17').click(function () {
+
+  //    		$("#pre17").hide();
+
+		//    var mediaVideo = $("#bgdvid17").get(0);
+		//    var playv = $("#playv17");
+		//    if (mediaVideo.paused) {
+		//        mediaVideo.play();
+		//        playv.parent();
+		//    } else {
+		//        mediaVideo.pause();
+		//        playv.parent();
+		//   }
+		// });
+
+// 열여덟 번째 동영상
+
+     $('#play-pause-button18').click(function () {
+
+     		$("#pre18").hide();
+
+		   var mediaVideo = $("#bgdvid18").get(0);
+		   var playv = $("#playv18");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 열아홉 번째 동영상
+
+     $('#play-pause-button19').click(function () {
+
+     		$("#pre19").hide();
+
+		   var mediaVideo = $("#bgdvid19").get(0);
+		   var playv = $("#playv19");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 스무 번째 동영상
+
+  //    $('#play-pause-button20').click(function () {
+
+  //    		$("#pre20").hide();
+
+		//    var mediaVideo = $("#bgdvid20").get(0);
+		//    var playv = $("#playv20");
+		//    if (mediaVideo.paused) {
+		//        mediaVideo.play();
+		//        playv.parent();
+		//    } else {
+		//        mediaVideo.pause();
+		//        playv.parent();
+		//   }
+		// });
+
+// 스물한 번째 동영상
+
+  //    $('#play-pause-button21').click(function () {
+
+  //    		$("#pre21").hide();
+
+		//    var mediaVideo = $("#bgdvid21").get(0);
+		//    var playv = $("#playv21");
+		//    if (mediaVideo.paused) {
+		//        mediaVideo.play();
+		//        playv.parent();
+		//    } else {
+		//        mediaVideo.pause();
+		//        playv.parent();
+		//   }
+		// });
+
+// 스물두 번째 동영상
+
+     $('#play-pause-button22').click(function () {
+
+     		$("#pre22").hide();
+
+		   var mediaVideo = $("#bgdvid22").get(0);
+		   var playv = $("#playv22");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 스물세 번째 동영상
+
+  //    $('#play-pause-button23').click(function () {
+
+  //    		$("#pre23").hide();
+
+		//    var mediaVideo = $("#bgdvid23").get(0);
+		//    var playv = $("#playv23");
+		//    if (mediaVideo.paused) {
+		//        mediaVideo.play();
+		//        playv.parent();
+		//    } else {
+		//        mediaVideo.pause();
+		//        playv.parent();
+		//   }
+		// });
+
+// 스물네 번째 동영상
+
+  //    $('#play-pause-button24').click(function () {
+
+  //    		$("#pre24").hide();
+
+		//    var mediaVideo = $("#bgdvid24").get(0);
+		//    var playv = $("#playv24");
+		//    if (mediaVideo.paused) {
+		//        mediaVideo.play();
+		//        playv.parent();
+		//    } else {
+		//        mediaVideo.pause();
+		//        playv.parent();
+		//   }
+		// });
+
+// 스물다섯 번째 동영상
+
+  //    $('#play-pause-button25').click(function () {
+
+  //    		$("#pre25").hide();
+
+		//    var mediaVideo = $("#bgdvid25").get(0);
+		//    var playv = $("#playv25");
+		//    if (mediaVideo.paused) {
+		//        mediaVideo.play();
+		//        playv.parent();
+		//    } else {
+		//        mediaVideo.pause();
+		//        playv.parent();
+		//   }
+		// });
+
+// 스물여섯 번째 동영상
+
+     $('#play-pause-button26').click(function () {
+
+     		$("#pre26").hide();
+
+		   var mediaVideo = $("#bgdvid26").get(0);
+		   var playv = $("#playv26");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 스물일곱 번째 동영상
+
+  //    $('#play-pause-button27').click(function () {
+
+  //    		$("#pre27").hide();
+
+		//    var mediaVideo = $("#bgdvid27").get(0);
+		//    var playv = $("#playv27");
+		//    if (mediaVideo.paused) {
+		//        mediaVideo.play();
+		//        playv.parent();
+		//    } else {
+		//        mediaVideo.pause();
+		//        playv.parent();
+		//   }
+		// });
+
+// 스물여덟 번째 동영상
+
+     $('#play-pause-button28').click(function () {
+
+     		$("#pre28").hide();
+
+		   var mediaVideo = $("#bgdvid28").get(0);
+		   var playv = $("#playv28");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+// 스물아홉 번째 동영상
+
+     $('#play-pause-button29').click(function () {
+
+     		$("#pre29").hide();
+
+		   var mediaVideo = $("#bgdvid29").get(0);
+		   var playv = $("#playv29");
+		   if (mediaVideo.paused) {
+		       mediaVideo.play();
+		       playv.parent();
+		   } else {
+		       mediaVideo.pause();
+		       playv.parent();
+		  }
+		});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
